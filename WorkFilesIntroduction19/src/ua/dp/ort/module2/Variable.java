2b457eefe9ee7300ab1aab39d93097d48f041746
PC <PC@192.168.0.72>
2019-11-23 11:27:41 +0200

    Add class variables

---
+++ WorkFilesIntroduction19/src/ua/dp/ort/module2/Variable.java
@@ -0,0 +1,49 @@
+package ua.dp.ort.module2;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        // ќднострочный комментарий   //
+        // ћногострочный комментарий  /*  */
+        
+        //--------------------------------------------------        
+        int a = 100 + 1;
+        
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+        
+        //------------------------------------------------
+        
+        int ab = 10;
+        
+//        int мо€Ћюбима€ѕеременна€ = 100;
+//            System.out.println(мо€Ћюбима€ѕеременна€);
+        
+        //int myAppelVasia = 25;
+        //int myappelvasia = 25;
+        
+        //class name
+        //MyClassVasia
+        
+//        int f = 10;
+//        int j = 20;
+//        int l = j + f;
+//        System.out.println(f + j);
+        
+//        int vasiaAppels = 20;
+//        int mashaAppels = 30;
+//        int totalAppels = vasiaAppels + mashaAppels;
+//        System.out.println(totalAppels);
+        
+        
+        double t = 1.2;
+        
+        boolean isNewYear = false;
+        System.out.println(isNewYear);
+    }
+
+}
