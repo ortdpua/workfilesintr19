74317cbf5e3e4bcd061750dc6b1c5233faa8aa0d
PC <PC@192.168.0.72>
2019-11-23 13:31:31 +0200

    Add data types class

---
+++ WorkFilesIntroduction19/src/ua/dp/ort/module2/DataTypes.java
@@ -0,0 +1,43 @@
+package ua.dp.ort.module2;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        
+//        int a = 1999999999;
+//        int b = 1999999999;
+//        
+//        int c = a + b;
+//        System.out.println(c);
+        
+        
+//        int f = 100 / 0;
+        
+//        double d = -1.22;
+//        System.out.println(d);
+        
+        
+//        byte a1 = 8;
+//        byte b2 = 9;
+//        byte c1 = (byte) (a1 + b2);
+//        
+//       char cc = 10000;
+//       System.out.println(cc);
+//       
+//       char cc1 = '%';
+//       System.out.println(cc1);
+//       
+//       char cc2 = '\u7878';
+//       System.out.println(cc2);
+       
+        int диаметр«емли;
+        
+        double salary = 1.0;
+        float flt = 1.0F;
+        System.out.println(salary - 0.1);
+        
+        
+        //0.89999
+    }
+
+}
