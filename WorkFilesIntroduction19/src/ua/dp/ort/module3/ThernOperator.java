80ffa69f877c52bf70e1b27e4906ee56dc7c2697
PC <PC@192.168.0.102>
2019-12-21 17:35:03 +0200

    Add work files module 3 part 2

---
+++ WorkFilesIntroduction19/src/ua/dp/ort/module3/ThernOperator.java
@@ -0,0 +1,21 @@
+package ua.dp.ort.module3;
+
+public class ThernOperator {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a < b");
+        }
+
+        String s = (a < b) ? ("a < b") : ("a < b");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!! " + s);
+
+    }
+
+}
